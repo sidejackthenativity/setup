633a19f8313890854775984e17110d66b8abb05f
sidejackthenativity <70146213+sidejackthenativity@users.noreply.github.com>
2020-09-06 10:43:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,51 @@ sudo apt install openvpn
 sudo snap install jq  
 
 sudo apt  install golang-go
+git clone https://github.com/tomnomnom/httprobe.git
+
 go get -u github.com/tomnomnom/httprobe
+
+
+#install go
+if [[ -z "$GOPATH" ]];then
+echo "It looks like go is not installed, would you like to install it now"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+                yes)
+
+					echo "Installing Golang"
+					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+					sudo mv go /usr/local
+					export GOROOT=/usr/local/go
+					export GOPATH=$HOME/go
+					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+					source ~/.bash_profile
+					sleep 1
+					break
+					;;
+				no)
+					echo "Please install go and rerun this script"
+					echo "Aborting installation..."
+					exit 1
+					;;
+	esac	
+done
+fi
+
+#create a tools folder in ~/
+mkdir ~/tools
+cd ~/tools/
+
+echo "installing httprobe"
+go get -u github.com/tomnomnom/httprobe
+cd httprobe
+go build main.go
+mv main httprobe  
+cd ~/tools/
+echo "done"
