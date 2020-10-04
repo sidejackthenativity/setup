abc6c0b93218db4a6d4d13848639e10f52353c28
sidejackthenativity <70146213+sidejackthenativity@users.noreply.github.com>
2020-10-04 13:56:58 -0400

    Create install_basic.sh

---
+++ install_basic.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install curl
+\curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install ruby
+
+git clone https://github.com/rebootuser/LinEnum.git
